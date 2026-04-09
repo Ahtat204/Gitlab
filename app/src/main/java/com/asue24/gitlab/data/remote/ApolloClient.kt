1a3bee6752101f8bf03fa4b7f32e18758e0416a3
Lahcen <lahcen28ahtat@gmail.com>
2026-04-09 16:56:06 +0200

    succefully tested GraphQL API with Apollo and Gitlab access token(only for testing,the app will be using OAuth2)

--- app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
+++ app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
@@ -1,20 +1,30 @@
 package com.asue24.gitlab.data.remote
+
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.http.HttpRequest
+import com.apollographql.apollo.api.http.HttpResponse
+import com.apollographql.apollo.network.http.HttpInterceptor
+import com.apollographql.apollo.network.http.HttpInterceptorChain
 import com.apollographql.apollo.network.okHttpClient
+import com.asue24.gitlab.data.authentication.AccessToken.accessToken
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-class ApolloService  {
-    companion object{
-        fun setUpApolloClient(): ApolloClient {
-            val logging = HttpLoggingInterceptor()
-            logging.level = HttpLoggingInterceptor.Level.BODY
-            val okHttp = OkHttpClient
-                .Builder()
-                .addInterceptor(logging)
-            return ApolloClient.Builder()
-                .serverUrl("https://gitlab.com/api/graphql")
-                .okHttpClient(okHttp.build())
-                .build()
-        }
+
+object ApolloService {
+    public fun setUpApolloClient(): ApolloClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val okHttp = OkHttpClient.Builder().addInterceptor(logging)
+        return ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql")
+            .addHttpInterceptor(object : HttpInterceptor {
+                override suspend fun intercept(
+                    request: HttpRequest, chain: HttpInterceptorChain
+                ): HttpResponse {
+                    return chain.proceed(
+                        request.newBuilder().addHeader("Authorization", "Bearer $accessToken")
+                            .build()
+                    )
+                }
+            }).okHttpClient(okHttp.build()).build()
     }
 }
