7665801f7f8ba466759590628256505c737c0bdd
Lahcen <lahcen28ahtat@gmail.com>
2026-04-14 21:37:30 +0200

    implementing access token request if refresh token is not null and access token is null

--- app/src/main/java/com/asue24/gitlab/constants/SafeStore.kt
+++ app/src/main/java/com/asue24/gitlab/constants/SafeStore.kt
@@ -1,11 +1,21 @@
 package com.asue24.gitlab.constants
 
 import android.content.Context
+import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
 
-object SafeStore
-{
-    val Context.datastore by dataStore(
-        fileName = "gitlab-refresh-token", serializer = RefreshTokenSerializer
-    )
+private val Context.dataStore by dataStore(
+    fileName = "gitlab-refresh-token",
+    serializer = RefreshTokenSerializer
+)
+
+object AuthStorage {
+    private var instance: DataStore<GitlabRefreshToken>? = null
+    fun getInstance(context: Context): DataStore<GitlabRefreshToken> {
+        if (instance == null) {
+            instance = context.dataStore
+        }
+        return instance!!
+    }
 }
+
