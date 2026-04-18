f19d2c6f7ac210fa0eca5e0d1151ef5e0bf4ac7a
Lahcen <lahcen28ahtat@gmail.com>
2026-04-19 00:39:39 +0200

    clean up and using ViewModels to offload authentication from MainActivity

--- app/src/main/java/com/asue24/gitlab/data/repositories/AuthenticationRepository.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/AuthenticationRepository.kt
@@ -1,12 +1,18 @@
 package com.asue24.gitlab.data.repositories
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import com.asue24.gitlab.domain.utility.constants.AuthStorage
+import com.asue24.gitlab.domain.utility.constants.GitlabRefreshToken
+import com.asue24.gitlab.domain.utility.constants.Tokens
 import net.openid.appauth.AuthorizationService
 
 class AuthenticationRepository(
     val cntx: Context,
+    dataStore: DataStore<GitlabRefreshToken>?,
 ) {
+    private val accessToken = Tokens.accessToken
     val authService = AuthorizationService(cntx.applicationContext)
-    private var authenticationService: AuthorizationService? = null
+    val hasValidToken: Boolean = AuthStorage.getInstance(cntx) != null
 
 }
