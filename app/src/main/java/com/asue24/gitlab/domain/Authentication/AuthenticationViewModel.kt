7b32255748288244473c97a6290cc948210eb641
Lahcen <lahcen28ahtat@gmail.com>
2026-04-19 19:12:59 +0200

    cleanup and structring files

--- app/src/main/java/com/asue24/gitlab/domain/Authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/asue24/gitlab/domain/Authentication/AuthenticationViewModel.kt
@@ -1,9 +1,9 @@
-package com.asue24.gitlab.presentation.viewmodels
+package com.asue24.gitlab.domain.Authentication
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.asue24.gitlab.data.repositories.AuthenticationRepository
-import com.asue24.gitlab.domain.utility.constants.AuthStorage
+import com.asue24.gitlab.domain.Authentication.constants.AuthStorage
 import com.asue24.gitlab.presentation.navigation.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -18,7 +18,7 @@ class AuthenticationViewModel(private val authRepo: AuthenticationRepository) : 
 
     init {
         viewModelScope.launch {
-            val hasToken =AuthStorage.getAuthState(this@AuthenticationViewModel.authRepo.cntx).data.first().refreshToken != null
+            val hasToken = AuthStorage.getAuthState(this@AuthenticationViewModel.authRepo.cntx).data.first().refreshToken != null
 
             if (hasToken) {
                 _uiState.value = UiState.Authenticated
