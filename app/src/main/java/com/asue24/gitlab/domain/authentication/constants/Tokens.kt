68dd61e4dd352926e26814409aaa3f18f2415c9a
Lahcen <lahcen28ahtat@gmail.com>
2026-04-25 23:48:08 +0200

    working on the Project retrieval

--- app/src/main/java/com/asue24/gitlab/domain/authentication/constants/Tokens.kt
+++ app/src/main/java/com/asue24/gitlab/domain/authentication/constants/Tokens.kt
@@ -2,26 +2,4 @@ package com.asue24.gitlab.domain.authentication.constants
 object Tokens {
     var accessToken: String? = null
 }
-/*
-@Serializable
-data class GitlabRefreshToken(var refreshToken: String? = null)
-object RefreshTokenSerializer : Serializer<GitlabRefreshToken> {
-    override val defaultValue: GitlabRefreshToken
-        get() = GitlabRefreshToken()
 
-    override suspend fun readFrom(input: InputStream): GitlabRefreshToken = try {
-        val encryptedBytes = withContext(Dispatchers.IO) { input.readBytes() }
-        val decryptedBytes = CryptoUtility.decrypt(encryptedBytes)
-        Json.decodeFromString(decryptedBytes.decodeToString())
-    } catch (e: Exception) {
-        defaultValue
-    }
-
-    override suspend fun writeTo(t: GitlabRefreshToken, output: OutputStream) {
-        val json = Json.encodeToString(t)
-        val encryptedBytes = CryptoUtility.encrypt(json.toByteArray())
-        withContext(Dispatchers.IO) {
-            output.use { it.write(encryptedBytes) }
-        }
-    }
-}*/
