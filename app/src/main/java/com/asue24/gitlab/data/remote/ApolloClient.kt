703af8e6d8c91d6c988ce6e1282bb30cef257429
Lahcen <lahcen28ahtat@gmail.com>
2026-04-27 21:17:23 +0200

    update ProjectsListQuery to return only contributed Projects
    # Conflicts:
    #	app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt

--- app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
+++ app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
@@ -1,25 +1,30 @@
 package com.asue24.gitlab.data.remote
 
-import android.util.Log
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.cache.normalized.api.MemoryCacheFactory
-import com.apollographql.apollo.cache.normalized.logCacheMisses
-import com.apollographql.apollo.cache.normalized.normalizedCache
+import com.apollographql.apollo.api.http.HttpRequest
+import com.apollographql.apollo.api.http.HttpResponse
+import com.apollographql.apollo.network.http.HttpInterceptor
+import com.apollographql.apollo.network.http.HttpInterceptorChain
 import com.apollographql.apollo.network.okHttpClient
 import com.asue24.gitlab.domain.authentication.constants.Tokens
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
 object ApolloService {
-    private val cacheFactory = MemoryCacheFactory(10 * 1024 * 1024, expireAfterMillis = 600000)
-    val client: ApolloClient by lazy {
-        val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.HEADERS
-        }).addInterceptor(AuthenticationInterceptor()).build()
-
-        ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql").addHttpHeader("Authorization", "Bearer ${Tokens.accessToken}")
-            .okHttpClient(okHttpClient)
-            .logCacheMisses({ res -> Log.w("CacheMiss", "object wasn't found in Cache${res}") })
-            .normalizedCache(cacheFactory, writeToCacheAsynchronously = false).build()
+    public fun setUpApolloClient(): ApolloClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val okHttp = OkHttpClient.Builder().addInterceptor(logging)
+        return ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql")
+            .addHttpInterceptor(object : HttpInterceptor {
+                override suspend fun intercept(
+                    request: HttpRequest, chain: HttpInterceptorChain
+                ): HttpResponse {
+                    return chain.proceed(
+                        request.newBuilder().addHeader("Authorization", "Bearer ${Tokens.accessToken}")
+                            .build()
+                    )
+                }
+            }).okHttpClient(okHttp.build()).build()
     }
 }
