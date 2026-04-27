406d4e1a907700fc46f8462311caee77de4c2d27
Lahcen <lahcen28ahtat@gmail.com>
2026-04-28 01:27:09 +0200

    WIP , injected ViewModel into the UI

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -1,26 +1,29 @@
 package com.asue24.gitlab.data.repositories.project
 
 import com.asue24.gitlab.GetMyProjectsQuery
-import com.asue24.gitlab.GetMyProjectsQuery.Data
 import com.asue24.gitlab.GetRepoTreeQuery
-import com.asue24.gitlab.GetRepoTreeQuery.Node
 import com.asue24.gitlab.data.remote.ApolloService
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import okhttp3.internal.notify
+import java.util.concurrent.ConcurrentHashMap
 
+/**
+ * this is a singleton object , which guarantees the ConcurrentHashMap will live throughout the Application lifecycle
+ */
 class ProjectRepositoryImpl() : ProjectRepository {
+    /**
+     * Extremely fast project lookup to avoid frequent API requests .
+     */
+    private val cache: ConcurrentHashMap<String, GetRepoTreeQuery.Project> = ConcurrentHashMap()
     private val gitlab = ApolloService.setUpApolloClient()
 
     /**
      * @brief Streams contributed projects from GitLab.
      * Uses context preservation and structured concurrency.
      */
-    override suspend fun getAllProjects(): Flow<Data> {
+    override fun getAllProjects(): Flow<GetMyProjectsQuery.Data> {
         val result = gitlab.query(GetMyProjectsQuery()).toFlow()
         val response = result.map { resp ->
             if (resp.hasErrors()) {
@@ -31,13 +34,10 @@ class ProjectRepositoryImpl() : ProjectRepository {
 
         return response.flowOn(Dispatchers.IO)
     }
-
-    override suspend fun getProjectById(id: String, path: String): Node {
-        val scope= CoroutineScope(Dispatchers.IO)
-        scope.async {
-            val result = gitlab.query(GetRepoTreeQuery(id, path)).notify()
-
-        }
+    override suspend fun getProjectById(id: String, path: String): GetRepoTreeQuery.Project {
+        return cache[id] ?: gitlab.query(GetRepoTreeQuery(id, path))
+            .execute()
+            .dataAssertNoErrors.project!!
+            .also { cache[id] = it }
     }
-
 }
