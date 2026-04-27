8d36beb91a6e9a1928d3f02e8a8debfd4611177b
Lahcen <lahcen28ahtat@gmail.com>
2026-04-27 23:59:29 +0200

    introducing a ConcurrentHashMap to act as project cache

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package com.asue24.gitlab.data.repositories.project
 
 import com.asue24.gitlab.GetMyProjectsQuery
+import com.asue24.gitlab.GetMyProjectsQuery.Data
+import com.asue24.gitlab.GetRepoTreeQuery
+import com.asue24.gitlab.GetRepoTreeQuery.Node
 import com.asue24.gitlab.data.remote.ApolloService
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import okhttp3.internal.notify
 
 class ProjectRepositoryImpl() : ProjectRepository {
     private val gitlab = ApolloService.setUpApolloClient()
@@ -14,7 +20,7 @@ class ProjectRepositoryImpl() : ProjectRepository {
      * @brief Streams contributed projects from GitLab.
      * Uses context preservation and structured concurrency.
      */
-    override fun getAllProjects(): Flow<GetMyProjectsQuery.Data> {
+    override suspend fun getAllProjects(): Flow<Data> {
         val result = gitlab.query(GetMyProjectsQuery()).toFlow()
         val response = result.map { resp ->
             if (resp.hasErrors()) {
@@ -26,8 +32,12 @@ class ProjectRepositoryImpl() : ProjectRepository {
         return response.flowOn(Dispatchers.IO)
     }
 
-    override fun getProjectById(id: String): GetMyProjectsQuery.ContributedProjects {
-        TODO("Not yet implemented")
+    override suspend fun getProjectById(id: String, path: String): Node {
+        val scope= CoroutineScope(Dispatchers.IO)
+        scope.async {
+            val result = gitlab.query(GetRepoTreeQuery(id, path)).notify()
+
+        }
     }
 
 }
