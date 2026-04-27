d95853e00ada0000c7e64585262bd6a96e001298
Lahcen <lahcen28ahtat@gmail.com>
2026-04-27 21:09:21 +0200

    GetMyProfile Method

--- app/src/main/java/com/asue24/gitlab/data/repositories/profile/ProfileRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/profile/ProfileRepositoryImpl.kt
@@ -1,3 +1,22 @@
 package com.asue24.gitlab.data.repositories.profile
 
-class ProfileRepositoryImpl {}
+import com.asue24.gitlab.GetMyProfileQuery
+import com.asue24.gitlab.data.remote.ApolloService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+
+class ProfileRepositoryImpl : ProfileRepository {
+    private val gitlab = ApolloService.setUpApolloClient()
+    override fun getMyProfile(): Flow<GetMyProfileQuery.Data> {
+        val result = gitlab.query(GetMyProfileQuery()).toFlow()
+        val response = result.map { res ->
+            if (res.hasErrors()) {
+                throw RuntimeException("GraphQL Errors: ${res.errors}")
+            }
+            res.dataAssertNoErrors
+        }
+        return response.flowOn(Dispatchers.IO)
+    }
+}
