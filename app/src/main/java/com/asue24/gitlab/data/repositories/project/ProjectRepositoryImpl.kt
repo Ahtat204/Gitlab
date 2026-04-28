1dfa11f937213a7bb1a0de0f498f2b1014bacafb
Lahcen <lahcen28ahtat@gmail.com>
2026-04-28 02:38:27 +0200

    moving to Apollo Two-Levels Normalized Cache to avoid manual Managing the ConcurrentHashMap consistency

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -7,16 +7,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import java.util.concurrent.ConcurrentHashMap
 
 /**
  * this is a singleton object , which guarantees the ConcurrentHashMap will live throughout the Application lifecycle
  */
 class ProjectRepositoryImpl() : ProjectRepository {
-    /**
-     * Extremely fast project lookup to avoid frequent API requests .
-     */
-    private val cache: ConcurrentHashMap<String, GetRepoTreeQuery.Project> = ConcurrentHashMap()
     private val gitlab = ApolloService.setUpApolloClient()
 
     /**
@@ -34,12 +29,8 @@ class ProjectRepositoryImpl() : ProjectRepository {
 
         return response.flowOn(Dispatchers.IO)
     }
-    override suspend fun getProjectById(id: String, path: String): GetRepoTreeQuery.Project? {
-        return cache[id] ?: gitlab.query(GetRepoTreeQuery(id, path))
-            .execute()
-            .dataAssertNoErrors.project
-            .also { it?.let {
-                cache[id]=it
-            } }
-    }
+
+    override suspend fun getProjectById(id: String, path: String): GetRepoTreeQuery.Project? =
+        gitlab.query(GetRepoTreeQuery(id, path)).execute().dataAssertNoErrors.project
+
 }
