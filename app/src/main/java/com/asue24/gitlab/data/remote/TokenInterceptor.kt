7a62340d2a4ac6c1cedd2690d278c5a5c9d65ca5
Lahcen <lahcen28ahtat@gmail.com>
2026-04-28 18:07:50 +0200

    adding an OkHttp interceptor to handle refresh access token

---
+++ app/src/main/java/com/asue24/gitlab/data/remote/TokenInterceptor.kt
@@ -0,0 +1,71 @@
+package com.asue24.gitlab.data.remote
+
+import android.content.Context
+import android.util.Log
+import com.asue24.gitlab.GitlabApp
+import com.asue24.gitlab.domain.authentication.constants.AuthStorage
+import com.asue24.gitlab.domain.authentication.constants.Tokens
+import com.asue24.gitlab.domain.authentication.constants.authStateStore
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+object Locker {
+    @OptIn(InternalCoroutinesApi::class)
+    private val locker = Any()
+}
+
+class TokenInterceptor() : Interceptor {
+    val authenticationService = Tokens.authService
+    @OptIn(InternalCoroutinesApi::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        val builder = request.newBuilder()
+        val token = Tokens.accessToken
+        if (token != null) {
+            builder.header("Authorization", "Bearer $token")
+        }
+        request = builder.build()
+        var response = chain.proceed(request)
+        if (response.code == 401) {
+            synchronized(Locker) {
+                val state = Tokens.CurrentAuthState
+                val accessToken = Tokens.accessToken
+                if (accessToken != null && accessToken.equals(token) && state != null && Tokens.authService != null) {
+                    val deferred = CompletableDeferred<String?>()
+                    runBlocking {
+                        state.performActionWithFreshTokens(Tokens.authService!!) { token, _, ex ->
+                            if (token != null && ex == null) {
+                                Tokens.accessToken = token
+                                Tokens.CurrentAuthState = state
+                                Tokens.authService = authenticationService
+                                deferred.complete(token)
+                                CoroutineScope(Dispatchers.IO).launch {
+                                   AuthStorage.getAuthState(context).updateData { state }
+                                }
+                            }
+                            if (ex != null) {
+                                deferred.completeExceptionally(ex)
+                            }
+                        }
+                        deferred.await()
+                    }
+                    builder.header("Authorization", "Bearer ${Tokens.accessToken}")
+                    request = builder.build()
+                    response = chain.proceed(request)
+                    if (response.code == 401) {
+                        Log.e("RefreshError", "couldn't refresh")
+                    }
+                }
+            }
+            response.close()
+        }
+        return response
+    }
+}
--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectDetailsScreen.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectDetailsScreen.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asue24.gitlab.presentation.viewmodels.ProjectViewModel
 
 @Composable
-fun ProjectDetailScreen(id: String,path:String, viewModel: ProjectViewModel) {
+fun ProjectDetailScreen(id: String,path:String) {
+    val viewModel= viewModel<ProjectViewModel>()
     // 1. Observe the State
     val project by viewModel.currentProject.collectAsStateWithLifecycle()
     LaunchedEffect (id) {
         viewModel.loadProject(id, path)
     }
     project?.let {
-        Text("Project: ${it.name}")
+        Text("Project: ${it.name}",color = Color.White)
     } ?: CircularProgressIndicator()
 }
