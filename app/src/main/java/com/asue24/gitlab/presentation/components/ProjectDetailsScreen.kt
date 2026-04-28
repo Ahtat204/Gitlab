81a3c605ecf8933ff1780356c3c2194d96442973
Lahcen <lahcen28ahtat@gmail.com>
2026-04-28 02:06:27 +0200

    solving projectID mismatch

--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectDetailsScreen.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectDetailsScreen.kt
@@ -5,19 +5,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.asue24.gitlab.presentation.viewmodels.ProjectViewModel
 
 @Composable
-fun ProjectDetailScreen(id: String) {
-    val viewModel= viewModel<ProjectViewModel>()
+fun ProjectDetailScreen(id: String,path:String, viewModel: ProjectViewModel) {
+    // 1. Observe the State
     val project by viewModel.currentProject.collectAsStateWithLifecycle()
     LaunchedEffect (id) {
-        viewModel.loadProject(id)
+        viewModel.loadProject(id, path)
     }
     project?.let {
-        Text("Project: ${it.name}",color = Color.White)
+        Text("Project: ${it.name}")
     } ?: CircularProgressIndicator()
 }
