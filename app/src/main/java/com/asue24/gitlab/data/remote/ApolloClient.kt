cf542891d3e41249815b0f62d5a2a35f6b5e8d99
Lahcen <lahcen28ahtat@gmail.com>
2026-04-29 19:54:27 +0200

    the problem of the cache was that I've been creating a new Instance of ApolloClient Every time with setUpApolloClient, I Completly overlooked it

--- app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
+++ app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
@@ -1,30 +1,23 @@
 package com.asue24.gitlab.data.remote
 
-import android.util.Log
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.api.MemoryCacheFactory
-import com.apollographql.apollo.cache.normalized.logCacheMisses
+import com.apollographql.apollo.cache.normalized.fetchPolicy
 import com.apollographql.apollo.cache.normalized.normalizedCache
-import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.apollo.network.okHttpClient
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import java.io.File
 
 object ApolloService {
-    private val sqlNormalizedCacheFactory = SqlNormalizedCacheFactory("apollo.db")
-    private val cacheFactory =
-        MemoryCacheFactory(10 * 1024 * 1024, expireAfterMillis = 100000) //10MB of cache , TTL=1 min
+    private val cacheFactory = MemoryCacheFactory(10 * 1024 * 1024, expireAfterMillis = 600000)
+    val client: ApolloClient by lazy {
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).addInterceptor(AuthenticationInterceptor()).build()
 
-    fun setUpApolloClient(): ApolloClient {
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.BODY
-        val okHttp = OkHttpClient.Builder().addInterceptor(logging)
-        return ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql")
-            .logCacheMisses({ res -> Log.w("CacheMiss", "object wasn't found in Cache${res}") })
-            .normalizedCache(
-                cacheFactory, cacheKeyGenerator,writeToCacheAsynchronously=false
-            ).okHttpClient(okHttp.addInterceptor(TokenInterceptor()).build()).build()
+        ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql")
+            .normalizedCache(cacheFactory, writeToCacheAsynchronously = false)
+            .okHttpClient(okHttpClient).fetchPolicy(FetchPolicy.CacheFirst).build()
     }
-
 }
