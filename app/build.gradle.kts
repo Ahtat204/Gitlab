b5a13b73583be32847d404675adb569840831ab2
Lahcen <lahcen28ahtat@gmail.com>
2026-05-03 00:30:27 +0200

    enhancing the Home Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ apollo {
     }
 }
 dependencies {
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -101,5 +102,7 @@ dependencies {
     implementation(libs.androidx.material.icons.core)
     implementation(libs.apollo.normalized.cache)
     implementation(libs.apollo.http.cache)
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt:coil-svg:2.7.0")
 
 }
--- app/src/main/java/com/asue24/gitlab/presentation/components/TopBar.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/TopBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.asue24.gitlab.presentation.ui.theme.Orange
 
 /**
  * the name represent the name of the screen ,as this Bar will contain useful buttons (like search and refresh )
@@ -32,21 +33,14 @@ fun TopBar(name: String) {
             color = Color.White,
             fontSize = 27.sp
         )
-        IconButton(onClick = {}, modifier = Modifier.weight(0.5f)) {
+        IconButton(onClick = {}, modifier = Modifier.weight(0.2f)) {
             Icon(
                 Icons.Filled.Search,
                 contentDescription = null,
-                tint = Color.Blue,
+                tint = Color.White,
                 modifier = Modifier
             )
         }
-        IconButton(onClick = {}, modifier = Modifier.weight(0.5f)) {
-            Icon(
-                Icons.Filled.Search,
-                contentDescription = null,
-                tint = Color.Blue,
-                modifier = Modifier
-            )
-        }
+
     }
 }
