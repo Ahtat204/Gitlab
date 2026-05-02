61402cad7653b36861d5059f7eb3c3a89068f0f1
Lahcen <lahcen28ahtat@gmail.com>
2026-05-02 22:34:08 +0200

    added a top bar on the Home Screen with Search button

--- app/src/main/java/com/asue24/gitlab/presentation/components/WorkItems.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/WorkItems.kt
@@ -1,17 +1,23 @@
 package com.asue24.gitlab.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 /**
  * this Component shows a List of Work Items on your Gitlab , examples: MRs , Issues ,To-do items,Starred Projects...
  */
 @Composable
-public fun MyWorkItems(navController: NavController) {
+ fun MyWorkItems(navController: NavController) {
     val myWorkItems = listOf("issues", "merge requests", "To-do items")
 
-    Column() {
+    Column(modifier = Modifier.padding(10.dp), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
         myWorkItems.forEach { item ->
         WorkItem(item) {
             navController.navigate(item)
