f6c29d24f3c4639aa8ce9f331f34331067707b34
Lahcen <lahcen28ahtat@gmail.com>
2026-05-02 22:34:08 +0200

    added a top bar on the Home Screen with Search button

--- app/src/main/java/com/asue24/gitlab/presentation/screens/Home.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/screens/Home.kt
@@ -1,16 +1,25 @@
 package com.asue24.gitlab.presentation.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.asue24.gitlab.presentation.components.MyWorkItems
+import com.asue24.gitlab.presentation.components.TopBar
 
 @Composable
 fun Home(navController: NavHostController) {
-    MyWorkItems(navController)
+    Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally){
+        TopBar("Home")
+        MyWorkItems(navController)
+    }
 }
