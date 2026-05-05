85b90d57ec881802a419c5898d8d0a8c671727e4
Lahcen <lahcen28ahtat@gmail.com>
2026-05-05 21:56:28 +0200

    fixed UI overlapping with status bar

--- app/src/main/java/com/asue24/gitlab/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/activities/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.asue24.gitlab.presentation.activities
 
-import android.R.attr.path
 import android.os.Bundle
 import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.asue24.gitlab.domain.authentication.constants.Tokens
 import com.asue24.gitlab.presentation.components.BottomBar
@@ -20,9 +22,12 @@ import java.io.File
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+       // enableEdgeToEdge(statusBarStyle =SystemBarStyle.dark(Color.BLACK),SystemBarStyle.auto(Color.BLACK, Color.BLACK))
         Tokens.context = application
         val mFolder = File(Environment. getExternalStorageDirectory().path+"gitlab/httpCache")
         if (!mFolder.exists()) {
@@ -31,10 +36,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             GitlabTheme(darkTheme = true) {
-                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = { androidx.compose.material3.TopAppBar(title = {
+                    Text(
+                        navController.currentDestination?.route.toString()
+                    )
+                }) },bottomBar = {
                     BottomBar(navController)
                 }, floatingActionButtonPosition = FabPosition.End) { x ->
-                    BottomNavigationgraph(navController)
+                    BottomNavigationgraph(navController,x)
                 }
             }
         }
