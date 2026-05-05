393de6310d6a2e50370994b9da253da06f176300
Lahcen <lahcen28ahtat@gmail.com>
2026-05-05 21:56:28 +0200

    fixed UI overlapping with status bar

--- app/src/main/java/com/asue24/gitlab/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/navigation/NavigationGraph.kt
@@ -1,5 +1,6 @@
 package com.asue24.gitlab.presentation.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -8,12 +9,12 @@ import com.asue24.gitlab.presentation.screens.Home
 
 @Composable
 fun BottomNavigationgraph(
-    navController: NavHostController,
+    navController: NavHostController,x: PaddingValues
 ) {
 
     NavHost(navController = navController, startDestination = BottomBarScreen.Home.route) {
         composable(route = BottomBarScreen.Home.route) {
-        Home(navController)
+        Home(navController,x)
         }
         composable(route = BottomBarScreen.Projects.route) {
         //LoginScreen(navController)
--- app/src/main/java/com/asue24/gitlab/presentation/screens/Home.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/screens/Home.kt
@@ -3,7 +3,9 @@ package com.asue24.gitlab.presentation.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,15 +15,15 @@ import com.asue24.gitlab.presentation.components.MyWorkItems
 import com.asue24.gitlab.presentation.components.TopBar
 
 @Composable
-fun Home(navController: NavHostController) {
+fun Home(navController: NavHostController,x: PaddingValues) {
     Column(
-        modifier = Modifier
+        modifier = Modifier.padding(x)
             .fillMaxHeight()
             .background(Color.Black),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TopBar("Home")
+
         MyWorkItems(navController)
     }
 }
