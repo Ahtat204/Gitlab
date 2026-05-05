fce74b60beee7751958d2af603f67ba80a6d5936
Lahcen <lahcen28ahtat@gmail.com>
2026-05-05 23:18:16 +0200

    another Apollo CacheMissException

--- app/src/main/java/com/asue24/gitlab/presentation/screens/Projects.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/screens/Projects.kt
@@ -1,2 +1,11 @@
 package com.asue24.gitlab.presentation.screens
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.asue24.gitlab.presentation.components.ProjectList
+
+@Composable
+fun ProjectsScreen(navController: NavHostController,x: PaddingValues){
+    ProjectList(x)
+}
--- app/src/main/java/com/asue24/gitlab/presentation/viewmodels/ProjectViewModel.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/viewmodels/ProjectViewModel.kt
@@ -6,6 +6,7 @@ import com.asue24.gitlab.GetMyProjectsQuery.Node
 import com.asue24.gitlab.GetRepoTreeQuery
 import com.asue24.gitlab.data.repositories.project.ProjectRepository
 import com.asue24.gitlab.data.repositories.project.ProjectRepositoryImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -20,6 +21,17 @@ class ProjectViewModel : ViewModel() {
     private val projectRepository: ProjectRepository = ProjectRepositoryImpl()
     private val _projects = MutableStateFlow<List<Node>>(emptyList())
     val projects: StateFlow<List<Node>> = _projects.asStateFlow()
+
+
+    fun loadAllProjects() {
+        viewModelScope.launch(Dispatchers.IO) {
+            projectRepository.getAllProjects().collect { data ->
+                val newNodes =
+                    data.currentUser?.projectMemberships?.nodes?.filterNotNull() ?: emptyList()
+                _projects.value = newNodes
+            }
+        }
+    }
     fun loadProject(id: String) {
         viewModelScope.launch {
             val project = projectRepository.getProjectById(id)
