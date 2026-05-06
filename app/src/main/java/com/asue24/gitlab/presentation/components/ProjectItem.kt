ade2ba4d3c5a45e3431ee879dce91c046b5ce02d
Lahcen <lahcen28ahtat@gmail.com>
2026-05-06 13:36:42 +0200

    fixed CircularProgressIndicator Alignment

--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
@@ -1,6 +1,8 @@
 package com.asue24.gitlab.presentation.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,28 +12,47 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.asue24.gitlab.GetMyProjectsQuery
+import com.asue24.gitlab.presentation.ui.theme.Orange
+import com.asue24.gitlab.presentation.ui.theme.customFontFamily
 
 @Composable
-fun ProjectItem(project: GetMyProjectsQuery.Node) {
+fun ProjectItem(data: GetMyProjectsQuery.Node) {
     Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .clickable { },
-        elevation = CardDefaults.cardElevation(4.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(0.dp)
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+            verticalArrangement = Arrangement.Center
         ) {
-            project.project?.name?.let {
+            data.project?.let { project ->
                 Text(
-                    text = it, fontSize = 18.sp, modifier = Modifier.weight(1f)
+                    text = project.name,
+                    fontSize = 13.sp,
+                    color = Orange,
+                    modifier = Modifier,
+                    fontFamily = customFontFamily
                 )
+                project.description?.let {
+                    Text(
+                        text = it,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        fontSize = 9.sp,
+                        color = Orange,
+                        modifier = Modifier.fillMaxWidth(0.5f),
+                        fontFamily = customFontFamily,
+
+                    )
+                }
             }
         }
     }
