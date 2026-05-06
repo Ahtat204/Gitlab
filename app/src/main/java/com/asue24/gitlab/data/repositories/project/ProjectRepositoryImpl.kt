387a7a587146b51d0107676eae8d6435b397dff1
Lahcen <lahcen28ahtat@gmail.com>
2026-05-06 12:36:23 +0200

    added a centralized CircularProgressIndicator() in the ProjectList screen and improving Project List item by using a Dedicated Composable instead of a Card to show Project in the list

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
+import com.apollographql.apollo.exception.CacheMissException
 import com.asue24.gitlab.GetMyProjectsQuery
 import com.asue24.gitlab.GetRepoTreeQuery
 import com.asue24.gitlab.data.remote.ApolloService
@@ -23,30 +24,17 @@ class ProjectRepositoryImpl : ProjectRepository {
      * @brief Streams contributed projects from GitLab.
      * Uses context preservation and structured concurrency.
      */
-    override fun getAllProjects(): Flow<GetMyProjectsQuery.Data> {
-        var result: Flow<ApolloResponse<GetMyProjectsQuery.Data>>
-        var response: Flow<GetMyProjectsQuery.Data>
-
-        if (Tokens.counter == 1) {
-            result = gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst).toFlow()
-            response = result.map { resp ->
-                if (resp.hasErrors()) {
-                    throw RuntimeException("GraphQL Errors: ${resp.errors}")
-                }
-                resp.dataAssertNoErrors
-            }
-            return response.flowOn(Dispatchers.IO)
-        }
-        result = gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.NetworkFirst).toFlow()
-        Tokens.counter=1
-        response = result.map { resp ->
-            if (resp.hasErrors()) {
-                throw RuntimeException("GraphQL Errors: ${resp.errors}")
-            }
-            resp.dataAssertNoErrors
-        }
-
-        return response.flowOn(Dispatchers.IO)
+    override suspend fun getAllProjects(): GetMyProjectsQuery.Data {
+       try{
+           val result = gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst).execute().dataAssertNoErrors
+           return result
+       }
+       catch(ex:Exception){
+           if(ex is CacheMissException){
+               return gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.NetworkFirst).execute().dataAssertNoErrors
+           }
+       }
+        return GetMyProjectsQuery.Data(null)
     }
 
     override suspend fun getProjectById(id: String): Flow<GetRepoTreeQuery.Data?> {
