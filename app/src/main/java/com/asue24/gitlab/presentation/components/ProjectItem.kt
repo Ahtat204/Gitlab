24776df50f87deb8af2a11d27e4880e347180981
Lahcen <lahcen28ahtat@gmail.com>
2026-05-06 14:06:36 +0200

    improved Projects Screen and add screenHots in README

--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
@@ -1,16 +1,13 @@
 package com.asue24.gitlab.presentation.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,21 +20,19 @@ import com.asue24.gitlab.presentation.ui.theme.customFontFamily
 @Composable
 fun ProjectItem(data: GetMyProjectsQuery.Node) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(0.dp)
+        modifier = Modifier.fillMaxWidth()
     ) {
         Column(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Center
+                .fillMaxWidth()
+                .padding(15.dp)
+                , verticalArrangement = Arrangement.Center
         ) {
             data.project?.let { project ->
                 Text(
                     text = project.name,
                     fontSize = 13.sp,
-                    color = Orange,
+                    color = Color.White,
                     modifier = Modifier,
                     fontFamily = customFontFamily
                 )
@@ -47,10 +42,9 @@ fun ProjectItem(data: GetMyProjectsQuery.Node) {
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
                         fontSize = 9.sp,
-                        color = Orange,
+                        color = Color.White,
                         modifier = Modifier.fillMaxWidth(0.5f),
                         fontFamily = customFontFamily,
-
                     )
                 }
             }
