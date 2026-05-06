0d05d7808fa1f7284655b76826f7b90168f18aa1
Lahcen <lahcen28ahtat@gmail.com>
2026-05-06 13:36:42 +0200

    fixed CircularProgressIndicator Alignment

--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectList.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectList.kt
@@ -1,19 +1,14 @@
 package com.asue24.gitlab.presentation.components
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,9 +17,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.asue24.gitlab.presentation.ui.theme.titleFont
 import com.asue24.gitlab.presentation.viewmodels.ProjectViewModel
 
 /**
@@ -39,44 +36,29 @@ fun ProjectList(x: PaddingValues) {
     val projectList by projectViewModel.projects.collectAsState()
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.End
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(x)
+            .background(Color.Black)
     ) {
         if (projectList.isEmpty()) {
-            Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(x).offset(160.dp, y = (190).dp)) { CircularProgressIndicator(modifier = Modifier.padding(x)) }
+            CircularProgressIndicator(modifier = Modifier.offset(160.dp, y = (190).dp))
         } else {
-            LazyColumn(
+            Text(
+                text = "Your Projects",
+                fontFamily = titleFont,
+                fontSize = 20.sp,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(x),
+            )
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
                 contentPadding = x,
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(projectList) { item ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable { },
-                        elevation = CardDefaults.cardElevation(4.dp)
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            item.project?.let { project ->
-                                Text(
-                                    text = project.name,
-                                    fontSize = 18.sp,
-                                    modifier = Modifier.weight(1f)
-                                )
-                            }
-                        }
-                    }
+                    ProjectItem(item)
                 }
             }
         }
-
     }
-
 }
