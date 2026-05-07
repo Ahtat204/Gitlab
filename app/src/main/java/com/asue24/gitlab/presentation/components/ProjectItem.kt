1cb7a6e8849ce1a996efaa26fbe142c1e8ae1d37
Lahcen <lahcen28ahtat@gmail.com>
2026-05-07 20:45:07 +0200

    loading user avatar in the project List , just like Github Mobile

--- app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/components/ProjectItem.kt
@@ -10,19 +10,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.toColorInt
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.asue24.gitlab.GetMyProjectsQuery
 import com.asue24.gitlab.presentation.ui.theme.Background
 import com.asue24.gitlab.presentation.ui.theme.customFontFamily
@@ -36,57 +42,78 @@ fun ProjectItem(data: GetMyProjectsQuery.Node) {
             .fillMaxHeight()
             .height(100.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .background(Background)
+        val projectI=data.project
 
+       Row(modifier = Modifier
+           .background(Color.Black), verticalAlignment = Alignment.Top) {
 
-        ) {
+           projectI?.let{
+               it.avatarUrl?.let {url->
+                   val avatar= "https://gitlab.com/$url"
+                   Log.d("UserAvatar",avatar)
+                   AsyncImage(
+                       model = ImageRequest.Builder(LocalContext.current).data(avatar) // Image URL
+                           .crossfade(true) // Smooth fade-in
+                           .build(),
+                       contentDescription = "Sample Image",
+                       modifier = Modifier
+                           .padding(10.dp)
+                           .size(40.dp)
+                           .clip(RoundedCornerShape(20.dp))
+                   )
+               }
 
-            data.project?.let { project ->
-
-                Text(
-                    text = project.name,
-                    fontSize = 17.sp,
-                    color = Color.White,
-                    modifier = Modifier.offset(10.dp,0.dp),
-                    fontFamily = customFontFamily
-                )
-                project.description?.let {
+           }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(Background)
+            ) {
+                projectI?.let { project ->
                     Text(
-                        text = it,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1,
-                        fontSize = 9.sp,
+                        text = project.name,
+                        fontSize = 17.sp,
                         color = Color.White,
-                        modifier = Modifier
-                            .fillMaxWidth(0.8f)
-                            .offset(10.dp, 0.dp),
-                        fontFamily = customFontFamily,
+                        modifier = Modifier.offset(10.dp, 0.dp),
+                        fontFamily = customFontFamily
                     )
-                }
-                val languages=project.languages
-                if(languages?.isNotEmpty() == true){
-                    val language=languages[0]
-                    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(14.dp,0.dp)){
-                        Language(language.color.toString().toColorInt())
-                        Log.d("Language color",language.color.toString())
+                    project.description?.let {
                         Text(
-                            text = language.name,
-                            fontSize = 11.sp,
+                            text = it,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            fontSize = 9.sp,
                             color = Color.White,
-                            modifier = Modifier.offset(10.dp, 0.dp),
-                            fontFamily = customFontFamily
+                            modifier = Modifier
+                                .fillMaxWidth(0.8f)
+                                .offset(10.dp, 0.dp),
+                            fontFamily = customFontFamily,
                         )
                     }
+                    val languages = project.languages
+                    if (languages?.isNotEmpty() == true) {
+                        val language = languages[0]
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(14.dp, 0.dp)
+                        ) {
+                            Language(language.color.toString().toColorInt())
+                            Log.d("Language color", language.color.toString())
+                            Text(
+                                text = language.name,
+                                fontSize = 11.sp,
+                                color = Color.White,
+                                modifier = Modifier.offset(10.dp, 0.dp),
+                                fontFamily = customFontFamily
+                            )
+                        }
                     }
-
                 }
-
             }
         }
+    }
+
     }
 
 
