6aa9138a0125ef0119f4d7f778c3a16c4bff6a00
Lahcen <lahcen28ahtat@gmail.com>
2026-05-14 17:51:20 +0200

    added Apollo DI and trying to add a Cache Interceptor to avoid try catch on CacheMissException

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -1,12 +1,12 @@
 package com.asue24.gitlab.data.repositories.project
 
 import android.util.Log
+import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
-import com.apollographql.apollo.exception.CacheMissException
+import com.apollographql.apollo.cache.normalized.watch
 import com.asue24.gitlab.GetMyProjectsQuery
 import com.asue24.gitlab.GetRepoTreeQuery
-import com.asue24.gitlab.data.remote.ApolloService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
@@ -16,29 +16,24 @@ import javax.inject.Inject
 /**
  * this is a singleton object , which guarantees the ConcurrentHashMap will live throughout the Application lifecycle
  */
-class ProjectRepositoryImpl @Inject constructor(): ProjectRepository {
-    private val gitlab = ApolloService.client
-
+class ProjectRepositoryImpl @Inject constructor(private val apolloClient: ApolloClient) :
+    ProjectRepository {
     /**
      * @brief Streams contributed projects from GitLab.
      * Uses context preservation and structured concurrency.
      */
-    override suspend fun getAllProjects(): GetMyProjectsQuery.Data {
-        try {
-            val result = gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst)
-                .execute().dataAssertNoErrors
-            return result
-        } catch (ex: Exception) {
-            if (ex is CacheMissException) {
-                return gitlab.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.NetworkFirst)
-                    .execute().dataAssertNoErrors
+    override suspend fun getAllProjects(): Flow<GetMyProjectsQuery.Data> =
+        apolloClient.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst).watch()
+            .map { response ->
+                if (response.hasErrors()) {
+                    Log.e(this.javaClass.toString(), response.errors.toString())
+                }
+                response.dataAssertNoErrors
             }
-        }
-        return GetMyProjectsQuery.Data(null)
-    }
 
     override suspend fun getProjectById(id: String): Flow<GetRepoTreeQuery.Data?> {
-        val result = gitlab.query(GetRepoTreeQuery(id)).fetchPolicy(FetchPolicy.CacheFirst).toFlow()
+        val result =
+            apolloClient.query(GetRepoTreeQuery(id)).fetchPolicy(FetchPolicy.CacheFirst).toFlow()
         val response = result.map { data ->
             if (data.hasErrors()) {
                 Log.e("GraphQL Error", data.exception.toString())
