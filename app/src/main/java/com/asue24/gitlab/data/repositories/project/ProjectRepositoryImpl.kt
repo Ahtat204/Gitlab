b2df17bea235dbe3ab74eb22368f5e33ef2b8834
Lahcen <lahcen28ahtat@gmail.com>
2026-05-14 18:47:33 +0200

    finally , a proper CacheMissException using flow.catch{} to switch to Network in case of the annoying CacheMissException

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -3,12 +3,15 @@ package com.asue24.gitlab.data.repositories.project
 import android.util.Log
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.FetchPolicy
+import com.apollographql.apollo.cache.normalized.apolloStore
 import com.apollographql.apollo.cache.normalized.fetchPolicy
-import com.apollographql.apollo.cache.normalized.watch
+import com.apollographql.apollo.exception.CacheMissException
 import com.asue24.gitlab.GetMyProjectsQuery
 import com.asue24.gitlab.GetRepoTreeQuery
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -22,14 +25,15 @@ class ProjectRepositoryImpl @Inject constructor(private val apolloClient: Apollo
      * @brief Streams contributed projects from GitLab.
      * Uses context preservation and structured concurrency.
      */
-    override suspend fun getAllProjects(): Flow<GetMyProjectsQuery.Data> =
-        apolloClient.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst).watch()
-            .map { response ->
-                if (response.hasErrors()) {
-                    Log.e(this.javaClass.toString(), response.errors.toString())
-                }
-                response.dataAssertNoErrors
+    override suspend fun getAllProjects(): Flow<GetMyProjectsQuery.Data> {
+        return flow {
+          emit(apolloClient.apolloStore.readOperation(GetMyProjectsQuery()))
+        }.catch { ex ->
+            if (ex is CacheMissException) {
+               emit( apolloClient.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst).execute().data!!)
             }
+        }
+    }
 
     override suspend fun getProjectById(id: String): Flow<GetRepoTreeQuery.Data?> {
         val result =
