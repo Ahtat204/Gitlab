36701f8fd9ef7a7596103ffeec6a105d993cff53
Lahcen <lahcen28ahtat@gmail.com>
2026-05-14 17:51:20 +0200

    added Apollo DI and trying to add a Cache Interceptor to avoid try catch on CacheMissException

--- app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
+++ app/src/main/java/com/asue24/gitlab/data/remote/ApolloClient.kt
@@ -3,10 +3,10 @@ package com.asue24.gitlab.data.remote
 import android.util.Log
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.api.MemoryCacheFactory
+import com.apollographql.apollo.cache.normalized.emitCacheMisses
 import com.apollographql.apollo.cache.normalized.logCacheMisses
 import com.apollographql.apollo.cache.normalized.normalizedCache
 import com.apollographql.apollo.network.okHttpClient
-import com.asue24.gitlab.domain.authentication.constants.Tokens
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
@@ -17,8 +17,8 @@ object ApolloService {
             level = HttpLoggingInterceptor.Level.HEADERS
         }).addInterceptor(AuthenticationInterceptor()).build()
 
-        ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql").addHttpHeader("Authorization", "Bearer ${Tokens.accessToken}")
-            .okHttpClient(okHttpClient)
+        ApolloClient.Builder().serverUrl("https://gitlab.com/api/graphql")
+            .okHttpClient(okHttpClient).addInterceptor(CacheInterceptor)
             .logCacheMisses({ res -> Log.w("CacheMiss", "object wasn't found in Cache${res}") })
             .normalizedCache(cacheFactory, writeToCacheAsynchronously = false).build()
     }
