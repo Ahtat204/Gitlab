b381924c12f362ca58f0a1a92032c5e98caa9e5d
Lahcen <lahcen28ahtat@gmail.com>
2026-05-14 15:56:51 +0200

    fixing hilt's weird errors

--- app/src/main/java/com/asue24/gitlab/presentation/viewmodels/ProjectViewModel.kt
+++ app/src/main/java/com/asue24/gitlab/presentation/viewmodels/ProjectViewModel.kt
@@ -1,24 +1,24 @@
 package com.asue24.gitlab.presentation.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.asue24.gitlab.GetMyProjectsQuery
-import com.asue24.gitlab.GetMyProjectsQuery.Node
 import com.asue24.gitlab.GetRepoTreeQuery
 import com.asue24.gitlab.data.repositories.project.ProjectRepository
-import com.asue24.gitlab.data.repositories.project.ProjectRepositoryImpl
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProjectViewModel : ViewModel() {
+@HiltViewModel
+class ProjectViewModel @Inject constructor(private val projectRepository: ProjectRepository) : ViewModel() {
     val currentProject = MutableStateFlow<GetRepoTreeQuery.Project?>(null)
 
     //TODO:this will be refactored to Dependency Injection,we're just testing now
-    private val projectRepository: ProjectRepository = ProjectRepositoryImpl()
+
     private val _projects = MutableStateFlow< GetMyProjectsQuery.CurrentUser?>(null)
     val projects: StateFlow< GetMyProjectsQuery.CurrentUser?> = _projects.asStateFlow()
     fun loadAllProjects() {
