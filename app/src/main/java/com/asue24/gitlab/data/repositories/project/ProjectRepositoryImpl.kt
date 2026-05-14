cac3b83d9abdbb1591b133cac5ecfa9a9ee75f55
Lahcen <lahcen28ahtat@gmail.com>
2026-05-14 22:59:22 +0200

    it looks like using watch for ractive UI is impossible because of the annoying CacheMissException

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepositoryImpl.kt
@@ -4,12 +4,15 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.annotations.ApolloExperimental
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
+import com.apollographql.apollo.cache.normalized.watch
 import com.apollographql.apollo.exception.CacheMissException
 import com.asue24.gitlab.GetMyProjectsQuery
 import com.asue24.gitlab.GetRepoTreeQuery
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import kotlin.coroutines.cancellation.CancellationException
 
@@ -23,30 +26,28 @@ class ProjectRepositoryImpl @Inject constructor(private val apolloClient: Apollo
      * Uses context preservation and structured concurrency.
      */
     @OptIn(ApolloExperimental::class)
-  override suspend fun getAllProjects(): Flow<GetMyProjectsQuery.Data> {
-    return flow {
-        // Directly call the suspend function — no async needed
-        val cacheResult = apolloClient.query(GetMyProjectsQuery())
-            .fetchPolicy(FetchPolicy.CacheFirst)
-            .execute()
-            .dataAssertNoErrors
+  override suspend fun getAllProjects(policy: FetchPolicy): Flow<GetMyProjectsQuery.Data> {
 
-        emit(cacheResult)
-    }.catch { ex ->
-        if (ex is CancellationException) throw ex
+      return flow {
+          try{
+              emitAll(
+                  apolloClient.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.CacheFirst)
+                      .watch().map { it.dataAssertNoErrors })
 
-        if (ex is CacheMissException) {
 
-            val networkResult = apolloClient.query(GetMyProjectsQuery())
-                .fetchPolicy(FetchPolicy.NetworkFirst)
-                .execute()
-                .data ?: throw IllegalStateException("No data returned from network")
+          }
+          catch (ex: Exception){
+              if(ex is CacheMissException){
+                  emitAll(
+                      apolloClient.query(GetMyProjectsQuery()).fetchPolicy(FetchPolicy.NetworkFirst)
+                          .watch().map { it.dataAssertNoErrors })
+              }
+          }
 
-            emit(networkResult)
-        } else {
-            throw ex
-        }
-    }
+}
+
+
+
 }
 
     override suspend fun getProjectById(id: String): Flow<GetRepoTreeQuery.Data?> {
