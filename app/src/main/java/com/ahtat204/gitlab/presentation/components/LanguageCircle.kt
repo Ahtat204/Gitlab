64b824b10e9a53aa73dcfc6dde69907620e6b564
Lahcen <lahcen28ahtat@gmail.com>
2026-05-16 16:14:31 +0200

    update and cleanup

---
+++ app/src/main/java/com/ahtat204/gitlab/presentation/components/LanguageCircle.kt
@@ -0,0 +1,29 @@
+package com.ahtat204.gitlab.presentation.components
+
+import androidx.compose.foundation.Canvasimport androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+
+
+
+@Composable
+fun Language(color: Int) {
+
+    Canvas(
+        modifier = Modifier
+    ) {
+        // Coordinates for the point (center of the canvas)
+        val center = Offset(1.0f, size.height / 2)
+
+        // Draw a single point
+        drawPoints(
+            points = listOf(center),
+            pointMode = PointMode.Points,
+            color = Color(color),
+            strokeWidth = 26f, // Controls point size
+            cap = StrokeCap.Round // Makes the point circular
+        )
+    }}
