291a31eb662eed1bbfc7ef61f480f908f80bd77a
Lahcen <lahcen28ahtat@gmail.com>
2026-05-16 13:22:57 +0200

    changed name of the query used for showing Project details and info ,and added additional fields to the query ;commit count,open MRs and issues count..;

--- app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepository.kt
+++ app/src/main/java/com/asue24/gitlab/data/repositories/project/ProjectRepository.kt
@@ -2,10 +2,10 @@ package com.asue24.gitlab.data.repositories.project
 
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.asue24.gitlab.GetMyProjectsQuery
-import com.asue24.gitlab.GetRepoTreeQuery
+import com.asue24.gitlab.GetProjectDetailsQuery
 import kotlinx.coroutines.flow.Flow
 
 interface ProjectRepository {
     suspend fun getAllProjects(policy: FetchPolicy): Flow<GetMyProjectsQuery.Data>
-    suspend fun getProjectById(id: String): Flow<GetRepoTreeQuery.Data?>
+    suspend fun getProjectById(id: String): Flow<GetProjectDetailsQuery.Data?>
 }
