cdfa6c8a121b74bb50d60c9f2b0fd0d5b2b4d27e
Lahcen <lahcen28ahtat@gmail.com>
2026-05-19 00:07:25 +0200

    fixup! solving some potential memory leaks that come from storing Android Context as a global singleton

--- app/src/main/java/com/ahtat204/gitlab/GitlabApp.kt
+++ app/src/main/java/com/ahtat204/gitlab/GitlabApp.kt
@@ -2,9 +2,13 @@ package com.ahtat204.gitlab
 
 import android.app.Application
 import android.content.Context
+import com.ahtat204.gitlab.domain.usecase.authentication.constants.Tokens
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GitlabApp : Application() {
-    val context: Context by lazy { this }
+    override fun onCreate() {
+        super.onCreate()
+        Tokens.initialize(this)
+    }
 }
