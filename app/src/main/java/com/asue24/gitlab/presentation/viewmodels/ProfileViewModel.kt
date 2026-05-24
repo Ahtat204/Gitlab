edd892fd0d66241358bb4abffcf9d366173d93c1
Lahcen <lahcen28ahtat@gmail.com>
2026-05-25 00:00:11 +0200

    refactor: removed the old asue24 package and moved the profile repository to the new ahtat204 package

--- app/src/main/java/com/asue24/gitlab/presentation/viewmodels/ProfileViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.asue24.gitlab.presentation.viewmodels
-
-import com.asue24.gitlab.data.repositories.profile.ProfileRepository
-import javax.inject.Inject
-
-class ProfileViewModel @Inject constructor(private val profileRepository: ProfileRepository) {}
