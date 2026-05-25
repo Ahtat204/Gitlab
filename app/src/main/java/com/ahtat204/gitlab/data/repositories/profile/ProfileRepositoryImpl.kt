1ec08bfeb9b3d03a7e5b60b676254d6750699049
Lahcen <lahcen28ahtat@gmail.com>
2026-05-25 18:28:27 +0200

    documented the ProfileRepositoryImpl

--- app/src/main/java/com/ahtat204/gitlab/data/repositories/profile/ProfileRepositoryImpl.kt
+++ app/src/main/java/com/ahtat204/gitlab/data/repositories/profile/ProfileRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.ahtat204.gitlab.data.repositories.profile
 
 import android.util.Log
 import com.ahtat204.gitlab.data.queries.GetMyProfileQuery
+import com.ahtat204.gitlab.data.queries.GetMyProjectsQuery
+import com.ahtat204.gitlab.data.queries.GetProjectDetailsQuery
+import com.ahtat204.gitlab.data.repositories.project.ProjectRepository
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
@@ -11,9 +14,45 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 import kotlin.coroutines.cancellation.CancellationException
-
+/**
+ * Implementation of [ProfileRepository] that integrates with GitLab via Apollo GraphQL.
+ *
+ * ## Overview
+ * - Provides reactive streams of user profile details using Kotlin [Flow].
+ * - Uses Apollo’s normalized caching with configurable [FetchPolicy].
+ * - Annotated with `@Inject` for dependency injection, ensuring a singleton lifecycle.
+ *
+ * ## Responsibilities
+ * - Fetch a User's profile with various details ,such as profile description , job title,linked accounts,etc...
+ * - Handle errors gracefully with logging and structured concurrency.
+ *
+ * ## Dependencies
+ * - [ApolloClient]: Executes GraphQL queries and manages caching.
+ * - [GetMyProfileQuery]: Auto‑generated query classes.
+ * - Kotlin Coroutines Flow: Enables reactive, cancellable streams.
+ */
 class ProfileRepositoryImpl @Inject constructor(private val apolloClient: ApolloClient) :
     ProfileRepository {
+    /**
+     * Retrieves the repository tree for a given project.
+     *
+     * @param policy The policy to use to fetch data ,ex:from cache first or from network first ,etc... .
+     * @return A [Flow] emitting [GetMyProfileQuery.Data] objects, or null if unavailable.
+     *
+     * ### Behavior
+     * - Executes [GetMyProfileQuery].
+     * - Uses Apollo’s normalized caching with [FetchPolicy.CacheFirst].
+     * - Emits results reactively via Flow.
+     * - Logs errors without terminating the stream.
+     *
+     * ### Example
+     * ```kotlin
+     * viewModelScope.launch {
+     *     profileRepository.getMyProfile(FetchPolicy.CacheFirst)
+     *         .collect { profile -> myProfile=it }
+     * }
+     * ```
+     */
     override fun getMyProfile(policy: FetchPolicy): Flow<GetMyProfileQuery.Data> {
         return apolloClient.query(GetMyProfileQuery()).fetchPolicy(policy).watch()
             .mapNotNull { it.data }.catch { ex ->
