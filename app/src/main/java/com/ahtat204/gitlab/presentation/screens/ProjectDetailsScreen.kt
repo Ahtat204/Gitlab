94b614c149de639eef143908b02388db6d27c2ac
Lahcen <lahcen28ahtat@gmail.com>
2026-05-27 00:43:04 +0200

    working on the screen that shows the details of the project the user clicked , showing its description , README(if availible) ....

--- app/src/main/java/com/ahtat204/gitlab/presentation/screens/ProjectDetailsScreen.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/screens/ProjectDetailsScreen.kt
@@ -1,23 +1,18 @@
-package com.ahtat204.gitlab.presentation.components
+package com.ahtat204.gitlab.presentation.screens
 
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ahtat204.gitlab.presentation.viewmodels.ProjectViewModel
 
 @Composable
-fun ProjectDetailScreen(id: String) {
-    val viewModel= viewModel<ProjectViewModel>()
+fun ProjectDetailScreen(id: String, viewModel: ProjectViewModel = hiltViewModel()) {
     val project by viewModel.currentProject.collectAsStateWithLifecycle()
-    LaunchedEffect (id) {
+    LaunchedEffect(id) {
         viewModel.loadProject(id)
     }
-    project?.let {
-        Text("Project: ${it.name}",color = Color.White)
-    } ?: CircularProgressIndicator()
+    project?.let {} ?: CircularProgressIndicator()
 }
