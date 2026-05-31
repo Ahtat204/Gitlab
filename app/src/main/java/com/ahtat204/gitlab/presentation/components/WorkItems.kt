7b651503e3c5e1dfe4b886214473cb07b50b6be2
Lahcen <lahcen28ahtat@gmail.com>
2026-05-31 11:43:58 +0200

    update:pull latest changes from main

--- app/src/main/java/com/ahtat204/gitlab/presentation/components/WorkItems.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/components/WorkItems.kt
@@ -5,6 +5,8 @@ package com.ahtat204.gitlab.presentation.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
