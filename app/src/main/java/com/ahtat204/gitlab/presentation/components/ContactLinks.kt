a1a32a9990466310f81508068d40928a3d677e72
Lahcen <lahcen28ahtat@gmail.com>
2026-06-01 17:40:31 +0200

    improved link rendering in Profile screen

--- app/src/main/java/com/ahtat204/gitlab/presentation/components/ContactLinks.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/components/ContactLinks.kt
@@ -1,32 +1,31 @@
 package com.ahtat204.gitlab.presentation.components
 
-import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Link
+import androidx.compose.material.Text
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.net.toUri
-import com.ahtat204.gitlab.data.queries.type.Color
-import com.ahtat204.gitlab.domain.usecase.authentication.constants.Tokens.context
-import com.ahtat204.gitlab.presentation.ui.theme.Orange
+import com.ahtat204.gitlab.presentation.ui.theme.topBarFont
 
 @Composable
-fun ContactLinks(vararg links: String) {
+fun ContactLinks(vararg links: Pair<String?, Int?>) {
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
@@ -36,32 +35,41 @@ fun ContactLinks(vararg links: String) {
     ) {
         links.forEach { link ->
             val annotatedText = buildAnnotatedString {
-                append("Check out ")
-                pushStringAnnotation(
-                    tag = "URL", annotation = link
-                )
-                withStyle(
-                    style = SpanStyle(
-                        color = androidx.compose.ui.graphics.Color.White, fontSize = 16.sp, textDecoration = TextDecoration.Underline
-                    )
-                ) {
-                    append(link)
+                link.first?.let {
+                    withLink(
+                        LinkAnnotation.Url(
+                            url = it, styles = TextLinkStyles(
+                                style = SpanStyle(
+                                    fontFamily = topBarFont,
+                                    fontSize = 14.sp,
+                                    color = Color.White,
+                                    textDecoration = TextDecoration.Underline
+                                )
+                            )
+                        )
+                    ) {
+                        append(link.first)
+                    }
                 }
             }
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(15.dp, 0.dp)
             ) {
-
-                ClickableText(
-                    text = annotatedText, onClick = { offset ->
-                        annotatedText.getStringAnnotations("URL", offset, offset).firstOrNull()
-                            ?.let { annotation ->
-                                val intent = Intent(Intent.ACTION_VIEW, annotation.item.toUri())
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                context.startActivity(intent)
-                            }
-                    })
+                link.second?.let {
+                    Icon(
+                        painter = painterResource(it),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(17.dp)
+                            .fillMaxSize()
+                    )
+                }
+                Text(
+                    annotatedText,
+                    modifier = Modifier.padding(10.dp, 10.dp),
+                )
             }
         }
     }
