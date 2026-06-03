feda3e4c593df5de1af51e033cdf3302d9f05845
Lahcen <lahcen28ahtat@gmail.com>
2026-06-03 19:23:23 +0200

    update from main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 }
 
 android {
-    namespace = "com.asue24.gitlab"
+    namespace = "com.ahtat204.gitlab"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.asue24.gitlab"
+        applicationId = "com.ahtat204.gitlab"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -50,11 +50,11 @@ android {
 }
 
 apollo {
-    service("service") {
-        packageName.set("com.asue24.gitlab")
+    service("gitlab") {
+        packageName.set("com.ahtat204.gitlab.data.queries")
         introspection {
             endpointUrl.set("https://gitlab.com/api/graphql")
-            schemaFile.set(file("app/src/main/graphql/com/pranav/schema.graphqls"))
+            schemaFile.set(file("app/src/main/graphql/com/ahtat204/schema.graphqls"))
             addTypename.set("always")
         }
     }
@@ -98,5 +98,4 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.apollo.normalized.cache.sqlite)
 }
