58b42e81839ed5e5ce9e1e5b838a652f9dbc894a
Lahcen <lahcen28ahtat@gmail.com>
2026-06-04 20:57:17 +0200

    improving bundle Workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -21,6 +25,10 @@ android {
         versionName = "1.0"
         manifestPlaceholders.putIfAbsent("appAuthRedirectScheme", "com.asue24.gitlab   ")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmm", Locale.getDefault())
+        val formattedDate = dateFormat.format(Date())
+        val fileName = "${rootProject.name}_v${versionName}_${versionCode}_${formattedDate}"
+        setProperty("archivesBaseName", rootProject.name)
     }
 
     buildTypes {
