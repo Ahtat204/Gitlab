e75bb781fb53b205c147778c4364b209b9ec1438
Lahcen <lahcen28ahtat@gmail.com>
2026-07-21 19:42:02 +0100

    screen:merge requests screen

--- app/src/main/java/com/ahtat204/gitlab/presentation/screens/project/MergeRequests.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/screens/project/MergeRequests.kt
@@ -1,8 +1,23 @@
 package com.ahtat204.gitlab.presentation.screens.project
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import com.ahtat204.gitlab.presentation.viewmodels.project.ProjectMRsViewModel
 
 @Composable
-fun MergeRequests(){
-
+fun MergeRequests(
+    project: String,
+    navController: NavController,
+    x: PaddingValues,
+    mRsViewModel: ProjectMRsViewModel = hiltViewModel()
+) {
+    val mergeRequests by mRsViewModel.mrs.collectAsStateWithLifecycle()
+    LaunchedEffect(project) {
+        mRsViewModel.loadProjectMRs(project)
+    }
 }
