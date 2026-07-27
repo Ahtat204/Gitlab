07b6a68a3aaed3ca0173f291ea712933dab1b9a0
Lahcen <lahcen28ahtat@gmail.com>
2026-07-27 16:48:32 +0100

    fix:closes #56

--- app/src/main/java/com/ahtat204/gitlab/presentation/viewmodels/project/ProjectViewModel.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/viewmodels/project/ProjectViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.ahtat204.gitlab.data.queries.GetMyPersonalProjectsQuery
 import com.ahtat204.gitlab.data.queries.GetProjectDetailsQuery
 import com.ahtat204.gitlab.data.remote.repositories.project.ProjectRepository
+import com.ahtat204.gitlab.domain.usecase.authentication.constants.Tokens
 import com.ahtat204.gitlab.presentation.components.withCacheFallback
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
