4cc246cf4b0cd65be4568615c1174aa7394bdcf9
Lahcen <lahcen28ahtat@gmail.com>
2026-08-29 14:15:29 +0100

    feat:adding poject name update graphql mutation

--- app/src/main/java/com/ahtat204/gitlab/presentation/screens/project/Repository.kt
+++ app/src/main/java/com/ahtat204/gitlab/presentation/screens/project/Repository.kt
@@ -2,16 +2,8 @@ package com.ahtat204.gitlab.presentation.screens.project
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,13 +11,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,11 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.ahtat204.gitlab.presentation.components.BranchesList
-import com.ahtat204.gitlab.presentation.components.FileBrowser
-import com.ahtat204.gitlab.presentation.components.RepositoryHead
-import com.ahtat204.gitlab.presentation.components.TreeItemCard
-import com.ahtat204.gitlab.presentation.components.iso8601ToRelative
+import com.ahtat204.gitlab.presentation.components.*
 import com.ahtat204.gitlab.presentation.viewmodels.project.repository.RepositoryViewModel
 
 /**
